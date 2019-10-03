02383c28f1809e3e14eed5e19111475ada53d04a
Michael Williams <mwilliams2964@gmail.com>
2019-10-02 19:05:16 -0700

    added moment.js logic

--- assets/schedule.js
+++ assets/schedule.js
@@ -1,6 +1,6 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
-    // Your web app's Firebase configuration
+  // Your web app's Firebase configuration
   var firebaseConfig = {
     apiKey: "AIzaSyBYu1dfKKMAR24cQrrfTfPYwelm3Fcmk1k",
     authDomain: "train-scheduler-724e3.firebaseapp.com",
@@ -20,7 +20,7 @@ $(document).ready(function() {
   let frequency = 0;
   let firstTrainTime = 0;
 
-  $(document).on("click", ".btn", function(event) {
+  $(document).on("click", ".btn", function (event) {
     event.preventDefault();
 
     trainName = $("#trainName").val().trim();
@@ -28,7 +28,7 @@ $(document).ready(function() {
     frequency = $("#frequency").val().trim();
     firstTrainTime = $("#firstTrainTime").val().trim();
     console.log(frequency);
-    
+
     let trainInfo =
     {
       train_name: trainName,
@@ -40,22 +40,52 @@ $(document).ready(function() {
       trainInfo
     )
   });
+  // let nextArrival = 
 
-  database.ref().on("child_added", function(childSnapshot) {
+  database.ref().on("child_added", function (childSnapshot) {
     console.log(childSnapshot.val().train_name);
     console.log(childSnapshot.val().destination);
     console.log(childSnapshot.val().frequency);
     console.log(childSnapshot.val().first_train_time);
+    let nextArrivalArr = childSnapshot.val().first_train_time.split(":");
+    console.log(nextArrivalArr);
+    let nextArrival = moment().hours(nextArrivalArr[0]).minutes(nextArrivalArr[1]);
+    console.log(nextArrival)
+    // moment.max takes two arguments- moment() and nextArrival. 
+    // It takes nextArrival time and subtracts from it the current time
+    let nextArrivalMax = moment.max(moment(), nextArrival);
+    let trainArrival;
+    let trainMin;
+    if (nextArrivalMax == nextArrival) {
+      trainArrival = nextArrival.format("hh:mm A");
+      trainMin = nextArrival.diff(moment(), "minutes");
+    } else {
+      var diffTime = moment().diff(nextArrival, "minutes");
+      var timeRemainder = diffTime % childSnapshot.val().frequency;
+      trainMin = childSnapshot.val().frequency - timeRemainder;
+      trainArrival = moment().add(trainMin, "m").format("hh:mm A")
+    }
     let row = $("<tr>");
     let tableData = $(
-    "<td>" + childSnapshot.val().train_name + "</td>" + 
-    "<td>" + childSnapshot.val().destination + "</td>" +
-    "<td>" + childSnapshot.val().frequency + "</td>" +
-    "<td>" + "hardcoded NEXT ARRIVAL" + "</td>" + 
-    "<td>" + "hardcoded MIN AWAY" + "</td>");
+      "<td>" + childSnapshot.val().train_name + "</td>" +
+      "<td>" + childSnapshot.val().destination + "</td>" +
+      "<td>" + childSnapshot.val().frequency + "</td>" +
+      "<td>" + trainArrival + "</td>" +
+      "<td>" + trainMin + "</td>");
     $(row).append(tableData);
     $("tbody").append(row);
 
   })
+
+  /* 
+  numMinPassedSinceTrainLeft = currentTime % frequency
+
+  minutesAway = frequency - numMinPassedSinceTrainLeft
+
+  nextArrival = currentTime + minutesAway
+  */
+
+
+
 
 })
